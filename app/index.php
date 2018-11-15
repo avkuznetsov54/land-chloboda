8cd558d909009794ad0bb56855a4d3834d03be5d
avkuznetsov54 <avkuznetsov54@gmail.com>
2018-11-15 13:06:37 +0700

    hooked fonts awesome

--- app/index.php
+++ app/index.php
@@ -11,13 +11,13 @@
 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	
+
 	<!-- Template Basic Images Start -->
 	<meta property="og:image" content="path/to/image.jpg">
 	<link rel="icon" href="img/favicon/favicon.ico">
 	<link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png">
 	<!-- Template Basic Images End -->
-	
+
 	<!-- Custom Browsers Color Start -->
 	<meta name="theme-color" content="#000">
 	<!-- Custom Browsers Color End -->
@@ -27,10 +27,38 @@
 </head>
 
 <body>
+	<div id="my-header">
+		<header class="site-header" style="background-image: url(img/top-bg.jpg);">
+			<div class="top-line">
+				<a href="#" class="logo"><img src="img/logo-1.svg" alt="Салон красоты"></a><i class="fas fa-ad"></i>
+				<div class="phone hidden-xs"><i class="fa fa-mobile"></i> +7 (233) 223-34-24</div>
+				<a href="#my-menu" class="hamburger hamburger--emphatic">
+					<span class="hamburger-box">
+						<span class="hamburger-inner"></span>
+					</span>
+				</a>
+				<!-- <nav id="my-menu">
+					<ul>
+						<li><a href="#">Парикмахерский сервис</a></li>
+						<li><a href="#">Косметологический сервис</a></li>
+						<li><a href="#">Маникюрный сервис</a></li>
+						<li><a href="#">Массаж и СПА</a></li>
+						<li><a href="#">Солярий</a></li>
+						<li><a href="#">Наши специалисты</a></li>
+						<li><a href="#">Новости</a></li>
+						<li><a href="#">Прайс-лист</a></li>
+						<li><a href="#">Наши сертификаты</a></li>
+						<li><a href="#">О салоне</a></li>
+					</ul>
+				</nav> -->
+			</div>
+			
+		</header>
+	</div>
 
-	
 
 	<script src="js/scripts.min.js"></script>
 
 </body>
+
 </html>
